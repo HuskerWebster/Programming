504a08cfd08b811e79d9682bc2460c73a52d32cf
HuskerWebster <57437283+HuskerWebster@users.noreply.github.com>
2020-03-04 12:17:30 +0000

    added things

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyString word = new MyString(new char[]{'c', 'a', 't'});
+        System.out.println(word);
+        System.out.println(word.length());
+
+        word.replace('t', 'r');
+        System.out.println(word);
+
+
     }
 }
--- src/com/company/MyString.java
+++ src/com/company/MyString.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class MyString {
+
+    char[] word;
+
+    public MyString(char[] word){
+        this.word = word;
+    }
+
+    public int length(){
+        return word.length;
+    }
+
+    public void replace(char c, char r){
+        char current = c;
+        char replace = r;
+        for(int i = 0; i < word.length; i++) {
+
+            if (word[i] == current) {
+                word[i] = replace;
+
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new String(word);
+    }
 }
